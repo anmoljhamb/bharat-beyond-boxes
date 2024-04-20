303871f6e03cccd0bb8a6c1772dfebc6c6e75989
Anmol Jhamb <talktoanmol@outlook.com>
2024-04-20 16:11:26 +0530

    Show HomePage depending on if the user is signed in or not

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound.tsx";
+import { useContext } from "react";
+import { AuthContext } from "./contexts/AuthContext.tsx";
+import HomePage from "./pages/HomePage.tsx";
 
 export default function App() {
+  const authContext = useContext(AuthContext)!;
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/"
+          element={authContext.isSignedIn ? <HomePage /> : <LandingPage />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
