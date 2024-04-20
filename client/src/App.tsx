e51da0806f89a862caeb7c84abcbb3ec824ed4f2
Anmol Jhamb <talktoanmol@outlook.com>
2024-04-20 15:32:34 +0530

    react-router-dom added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import NotFound from "./pages/NotFound.tsx";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
