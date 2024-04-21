ec95dff2ef48400ba5d78308fc730e7018907c4f
Anmol Jhamb <talktoanmol@outlook.com>
2024-04-21 06:50:10 +0530

    Sign In Page added

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -4,3 +4,4 @@ export { default as LandingCard1 } from "./landing-card-1.png";
 export { default as LandingCard2 } from "./landing-card-2.png";
 export { default as LandingCard3 } from "./landing-card-3.png";
 export { default as LandingCard4 } from "./landing-card-4.png";
+export { default as SignInPageImg } from "./sign-in-page.png";
--- client/src/pages/SignInPage.tsx
+++ client/src/pages/SignInPage.tsx
@@ -1,24 +1,39 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts";
+import { SignInPageImg } from "../assets";
 
 const SignInPage = () => {
   const auth = useAuth();
   const navigate = useNavigate();
 
   return (
-    <>
-      <div>SignInPage</div>
-      <button
-        disabled={auth.isLoading}
-        onClick={async () => {
-          await auth.signIn();
-          navigate("/");
-        }}
-        className="bg-black text-white p-4 disabled:opacity-50"
-      >
-        Sign In{" "}
-      </button>
-    </>
+    <section className="w-full flex h-screen">
+      <div className="w-3/5 h-screen relative">
+        <img
+          src={SignInPageImg}
+          className="w-full -z-10 h-full absolute object-cover"
+        />
+        <div className="bg-bg-secondary w-full h-full absolute z-0 opacity-65"></div>
+        <div className="pt-32 md:pt-14 h-screen flex flex-col justify-end items-center w-full overflow-hidden">
+          <h1 className="text-5xl w-5/6 text-center text-white z-10 my-5 font-bold">
+            Unlock your personalised India Tourism Experience
+          </h1>
+        </div>
+      </div>
+      <div className="pt-32 md:pt-14 h-screen flex flex-col justify-center items-center bg-bg-light w-2/5 overflow-hidden">
+        <h1 className="text-5xl text-bg-main my-5 font-bold">Namaste!</h1>
+        <button
+          disabled={auth.isLoading}
+          onClick={async () => {
+            await auth.signIn();
+            navigate("/");
+          }}
+          className="bg-white text-bg-main rounded-full text-2xl font-semibold p-4 disabled:opacity-50"
+        >
+          Sign In With Google
+        </button>
+      </div>
+    </section>
   );
 };
 
